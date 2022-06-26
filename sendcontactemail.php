8b469e16a19803e153b31781174f1866caa34bb2
lukeleisman <luke.leisman@gmail.com>
2022-06-25 20:07:32 -0500

    contact update

---
+++ sendcontactemail.php
@@ -0,0 +1,55 @@
+<?PHP
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+$name = $_POST['demo-name'];
+$email = $_POST['demo-email'];
+$message = $_POST['demo-message'];
+
+
+$to = "lrl62.55@gmail.com";
+$subject = "New Email Address for Mailing List";
+$headers = "From: $email\n";
+
+Email Address: $email";
+$user = "$email";
+$usersubject = "Thank You";
+$userheaders = "From: you@youremailaddress.com\n";
+$usermessage = "Thank you for submitting this form. I will reply as soon as possible. Here are the values you submitted: Name:$name Message:$message";
+
+
+if(IsInjected($email))
+{
+    echo "Bad email value!";
+    exit;
+}
+if(IsInjected($name))
+{
+    echo "Bad name value!";
+    exit;
+}
+
+mail($to,$subject,$message,$headers);
+//mail($user,$usersubject,$usermessage,$userheaders);
+?>
